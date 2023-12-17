a17732c477e12a926ce58adb3b2a2c71121f3073
laravel-king <laravelking@gmail.com>
2023-12-18 00:19:15 +0100

    Working on the main page

--- app/page.js
+++ app/page.js
@@ -1,35 +1,58 @@
 "use client"
-import Image from 'next/image'
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
 import {ResponsiveLine} from "@nivo/line";
+import {Badge} from "@/components/ui/badge";
+
 
 export default function Home() {
   return (
       <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
+        <h1 className="font-semibold text-lg">Accounts Overview</h1>
         <div className="grid lg:grid-cols-3 gap-4">
-          <Card>
+          <Card className="relative overflow-hidden text-white bg-gradient-to-r from-orange-600 to-orange-500 shadow-none">
             <CardHeader>
-              <CardTitle>Checking Account</CardTitle>
+              <CardTitle>New Account</CardTitle>
             </CardHeader>
             <CardContent>
-              <p>Balance: $5,000.00</p>
+              <p>New: +12%</p>
+              <HeartBeatIcon className="w-24 h-24 absolute top-2 right-0 opacity-30 text-green-50" />
+              <Badge variant="secondary" className="mt-3">
+                <span className="mr-1">
+                  +22% compared to last month
+                </span>
+                <TrendingUpIcon className="text-green-500 w-4 h-4" />
+              </Badge>
             </CardContent>
           </Card>
-          <Card>
+          <Card className="relative overflow-hidden text-white bg-gradient-to-r from-blue-500 to-blue-600 shadow-none">
             <CardHeader>
               <CardTitle>Savings Account</CardTitle>
             </CardHeader>
             <CardContent>
               <p>Balance: $10,000.00</p>
+              <SavingIcon className="w-24 h-24 absolute top-2 right-0 opacity-30 text-green-50" />
+              <Badge variant="secondary" className="mt-3">
+                <span className="mr-1">
+                  290 new saving accounts
+                </span>
+                 <TrendingUpIcon className="text-green-500 w-4 h-4" />
+              </Badge>
             </CardContent>
           </Card>
-          <Card>
+          <Card className="relative overflow-hidden text-white bg-gradient-to-r to-green-600 from-green-500 via-green-600/80 shadow-none">
             <CardHeader>
               <CardTitle>Investment Account</CardTitle>
             </CardHeader>
             <CardContent>
               <p>Balance: $15,000.00</p>
+              <DollarIcon className="w-24 h-24 absolute top-2 right-0 opacity-30 text-green-50" />
+              <Badge variant="secondary" className="mt-3">
+                <span className="mr-1">
+                  43 new projects 
+                </span>
+                <TrendingUpIcon className="text-green-500 w-4 h-4" />
+              </Badge>
             </CardContent>
           </Card>
         </div>
@@ -304,7 +327,7 @@ function LineChart2(props) {
                 data: [
                   {
                     x: 1,
-                    y: 283,
+                    y: 203,
                   },
                   {
                     x: 2,
@@ -312,11 +335,11 @@ function LineChart2(props) {
                   },
                   {
                     x: 3,
-                    y: 152,
+                    y: 162,
                   },
                   {
                     x: 4,
-                    y: 294,
+                    y: 194,
                   },
                   {
                     x: 5,
@@ -324,11 +347,11 @@ function LineChart2(props) {
                   },
                   {
                     x: 6,
-                    y: 172,
+                    y: 142,
                   },
                   {
                     x: 7,
-                    y: 24,
+                    y: 34,
                   },
                   {
                     x: 8,
@@ -499,5 +522,64 @@ function LineChart2(props) {
             role="application"
         />
       </div>
+  )
+}
+
+
+function HeartBeatIcon({props, className}) {
+  return (
+      <svg {...props} xmlns="http://www.w3.org/2000/svg" className={`icon icon-tabler icon-tabler-dollar ${className}` } width="24"
+           height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round"
+           strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M3 12h4.5l1.5 -6l4 12l2 -9l1.5 3h4.5"/>
+      </svg>
+  )
+}
+
+function DollarIcon({props, className}) {
+  return (
+      <svg {...props} xmlns="http://www.w3.org/2000/svg" className={`icon icon-tabler icon-tabler-currency-dollar ${className}` } width="24"
+           height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round"
+           strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2"/>
+        <path d="M12 3v3m0 12v3"/>
+      </svg>
+  )
+}
+
+
+function SavingIcon({props, className}) {
+  return (
+      <svg {...props} xmlns="http://www.w3.org/2000/svg" className={`icon icon-tabler icon-tabler-pig-money ${className}` } width="24" height="24"
+           viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round"
+           strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <path d="M15 11v.01"/>
+        <path d="M5.173 8.378a3 3 0 1 1 4.656 -1.377"/>
+        <path
+            d="M16 4v3.803a6.019 6.019 0 0 1 2.658 3.197h1.341a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-1.342c-.336 .95 -.907 1.8 -1.658 2.473v2.027a1.5 1.5 0 0 1 -3 0v-.583a6.04 6.04 0 0 1 -1 .083h-4a6.04 6.04 0 0 1 -1 -.083v.583a1.5 1.5 0 0 1 -3 0v-2l0 -.027a6 6 0 0 1 4 -10.473h2.5l4.5 -3h0z"/>
+      </svg>
+  )
+}
+
+function TrendingUpIcon(props) {
+  return (
+      <svg
+          {...props}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+      >
+        <polyline points="22 7 13.5 15.5 8.5 10.5 2 17" />
+        <polyline points="16 7 22 7 22 13" />
+      </svg>
   )
 }
--- components/SideBar.js
+++ components/SideBar.js
@@ -9,31 +9,31 @@ const SideBar = () => {
           <div className="flex flex-col gap-2">
               <AppLogo />
               <div className="flex-1">
-                  <nav className="grid items-start px-4 text-sm font-medium">
+                  <nav className="grid items-start px-4 text-sm font-medium gap-2">
                       <Link
-                          className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                          className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
                           href="#"
                       >
                           <HomeIcon className="h-4 w-4" />
                           Home
                       </Link>
                       <Link
-                          className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
+                          className="flex items-center gap-3 rounded-lg px-3 py-2 hover:bg-gray-100 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                           href="#"
                       >
                           <CreditCardIcon className="h-4 w-4" />
                           Accounts
-                          <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">3</Badge>
+                          <Badge className="ml-auto bg-red-500 flex h-6 w-6 shrink-0 items-center justify-center rounded-full">3</Badge>
                       </Link>
                       <Link
-                          className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                          className="flex items-center gap-3 rounded-lg px-3 py-2 hover:bg-gray-100 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                           href="#"
                       >
                           <MoveIcon className="h-4 w-4" />
                           Transfers
                       </Link>
                       <Link
-                          className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                          className="flex items-center gap-3 rounded-lg px-3 py-2 hover:bg-gray-100 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                           href="#"
                       >
                           <SettingsIcon className="h-4 w-4" />
